09416b647a3d081878ef1c20c9ddfb5c1abbb824
Dmitry <gworking7@gmail.com>
2024-12-23 23:48:20 +0300

    All tasks done

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import pyjokes
 from application.salary import calculate_salary
 from application.db.people import get_employees
+from datetime import datetime
 
 if __name__ == '__main__':
     employees = get_employees()
     for employee in employees:
         print(calculate_salary(employee))
+
+    print(datetime.now().date().strftime('%d-%m-%Y'))
+    print(pyjokes.get_joke())
